618bc91f3fade93abe68166bca850c839bbec4bd
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-06 17:30:09 +0300

    Add docstring

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""Adds two floats together and returns the sum."""
+
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floats together and returns the sum.
+
+    Args:
+      a (float): The first float number.
+      b (float): The second float number.
+
+    Returns (float):
+      The sum of a and b.
+"""
     return a + b
