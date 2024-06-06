0fa8cafd4e48b41e2a55e39d1396c716cf57155e
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-06 21:37:42 +0300

    Add task '3. Basic annotations - to string'

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+This script defines a function named to_str that converts a floating-point
+number to a string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function converts a floating-point number to a string representation.
+
+    Args:
+        n (float): The floating-point number to be converted to a string.
+
+    Returns:
+        str: The string representation of the floating-point number.
+    """
+
+    return str(n)
