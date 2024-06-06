e9d09df793777c78f776c88d86b30eba8af7efbd
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-06 19:15:53 +0300

    Add task '1. Basic annotations - concat'

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""concatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function concatenates two strings and returns the combined string.
+
+    Args:
+      str1: The first string.
+      str2: The second string.
+
+    Returns:
+      The combined string of str1 and str2.
+    """
+
+    return str1 + str2
