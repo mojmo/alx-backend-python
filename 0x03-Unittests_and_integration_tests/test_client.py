7cd5f426cdb4ec57d2947241d20e6dd1c9c4f530
mojtaba <mojtaba.mohamed187@gmail.com>
2024-06-30 19:20:18 +0300

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""This script defines unit tests for the `GithubOrgClient.org` method."""
+"""This script defines unit tests for the `GithubOrgClient.org` class."""
 
 from parameterized import parameterized
 import unittest
@@ -9,7 +9,7 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Test suite for the `GithubOrgClient.org` method."""
+    """Test suite for the `GithubOrgClient.org` class."""
 
     @parameterized.expand([
         ("google",),
@@ -29,3 +29,26 @@ class TestGithubOrgClient(unittest.TestCase):
         mock.assert_called_once_with(
             f"https://api.github.com/orgs/{org}"
         )
+
+    @patch('client.get_json')
+    def test_public_repos_url(self, mock):
+        """
+        Tests that `GithubOrgClient.public_repos_url` returns the
+        expected value.
+        """
+
+        # Mocked organization data
+        mocked_org_data = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+
+        mock.return_value = mocked_org_data
+
+        # Create an instance of GithubOrgClient
+        client = GithubOrgClient("google")
+
+        # Get the public repos URL
+        public_repos_url = client._public_repos_url
+
+        # Assert the expected URL
+        self.assertEqual(public_repos_url, mocked_org_data["repos_url"])
